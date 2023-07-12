492e7c8497a0cb7a8aee3d6d179a7de1080499b3
mnunezsa <mnunezsa95@gmail.com>
2023-07-12 12:44:06 -0400

    update / fix header urls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../Header/Header.js";
-// import Logo from "../../images/Logo.svg";
-// import Avatar from "../../images/avatar.svg";
 import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <div className="page">
+      <div className="page__wrapper">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,61 @@
 import React from "react";
 import "./Header.css";
+import logoPath from "../../images/logo.svg";
+import avatarPath from "../../images/avatar.svg";
 
 function Header(props) {
   const currentDate = new Date().toLocaleDateString("default", { month: "long", day: "numeric" });
+  const username = "Marlon Nunez";
+
   return (
-    <div>
-      <header className="header">
-        <div className="header__logo">
-          <div>
-            <img src="../../images/logo.svg" alt="WTWR Logo"></img>
-          </div>
-          <div>{currentDate}</div>
-        </div>
-        <div className="header__avatar">
-          <div>
-            <button className="header__nav__button" type="text">
-              +Add New Clothes
-            </button>
-          </div>
-          <div>Name</div>
-          <div>
-            <img src="../../images/avatar.svg" alt="Profile Avatar"></img>
-          </div>
-        </div>
-      </header>
-    </div>
+    <header className="header">
+      <div className="header__container">
+        <img src={logoPath} alt="WTWR logo" className="header__logo" />
+        <p className="header__date">{currentDate}</p>
+      </div>
+      <div className="header__nav">
+        <nav className="header__nav-section">
+          <ul className="header__nav-container">
+            <li>
+              <button className="header__nav__button" type="text">
+                + Add New Clothes
+              </button>
+            </li>
+            <li>
+              <div className="header__nav-link">
+                {username}
+                <img src={avatarPath} alt="Profile Avatar" className="header__nav-avatar"></img>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
+
+  //   return (
+  //     <div>
+  //       <header className="header">
+  //         <div className="header__container">
+  //           <div>
+  //             <img src={logoPath} alt="WTWR Logo"></img>
+  //           </div>
+  //           <p>{currentDate}</p>
+  //         </div>
+  //         <div className="header__avatar">
+  //           <div>
+  //             <button className="header__nav__button" type="text">
+  //               +Add New Clothes
+  //             </button>
+  //           </div>
+  //           <div>{username}</div>
+  //           <div>
+  //             <img src={avatarPath} alt="Profile Avatar"></img>
+  //           </div>
+  //         </div>
+  //       </header>
+  //     </div>
+  //   );
 }
 
 export default Header;
