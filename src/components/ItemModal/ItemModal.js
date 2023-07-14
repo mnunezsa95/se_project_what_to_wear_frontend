5e63d0c52d313865cd851b6cee6e309c1607cc82
mnunezsa <mnunezsa95@gmail.com>
2023-07-14 14:10:21 -0400

    ItemModal

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -3,12 +3,14 @@ import "./ItemModal.css";
 
 function ItemModal({ selectedCard, onClose }) {
   return (
-    <div className={`modal`}>
-      <div className="modal__content">
-        <img src={selectedCard.link} alt={selectedCard.name} />
-        <button className="modal__close-button" type="button" onClick={onClose} />
-        <p>{selectedCard.name}</p>
-        <p>Weather type: {selectedCard.weather}</p>
+    <div className="modal">
+      <div className="modal__content-card">
+        <img className="modal__image" src={selectedCard.link} alt={selectedCard.name} />
+        <button className="modal__close-button-image" type="button" onClick={onClose} />
+        <div className="modal__info">
+          <p className="modal__info-name">{selectedCard.name}</p>
+          <p className="modal__info-weather">Weather: {selectedCard.weather}</p>
+        </div>
       </div>
     </div>
   );
--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -5,7 +5,7 @@ function ModalWithForm({ children, modalName, title, buttonText = "Add garment",
     <div className={`modal modal_type_${modalName}`}>
       <div className="modal__content">
         <h3 className="modal__title">{title}</h3>
-        <button className="modal__close-button" type="button" onClick={onClose} />
+        <button className="modal__close-button " type="button" onClick={onClose} />
         <form className="modal__form">{children}</form>
         <button className="modal__submit-button" type="submit">
           {buttonText}
