70ba29b360b05190e01de083d7b71f50ff7cf6d2
mnunezsa <mnunezsa95@gmail.com>
2023-07-24 17:28:48 -0400

    update to UI for slider

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import headerLogo from "../../images/logo2.svg";
 import avatarImage from "../../images/avatar.svg";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch.js";
 
 function Header({ onCreateModal, currentLocation = "Location N/A" }) {
   const currentDate = new Date().toLocaleDateString("default", { month: "long", day: "numeric" });
@@ -14,6 +15,7 @@ function Header({ onCreateModal, currentLocation = "Location N/A" }) {
         <p className="header__date">{currentDate},</p>
         <p className="header__city">{currentLocation}</p>
       </div>
+      <ToggleSwitch />
       <div className="header__nav">
         <nav className="header__nav-section">
           <ul className="header__nav-container">
---
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ToggleSwitch.css";
+
+function ToggleSwitch() {
+  const [currentTemperatureUnit, handleToggleSwitchChange] = React.useState("C");
+  const handleChange = (e) => {
+    currentTemperatureUnit === "C" ? handleToggleSwitchChange("F") : handleToggleSwitchChange("C");
+  };
+  return (
+    <label className="switch">
+      <input className="switch__box" type="checkbox" onChange={handleChange}></input>
+      <span className={currentTemperatureUnit === "F" ? "switch__slider switch__slider-F" : "switch__slider switch__slider-C"}></span>
+      <p className={`switch__temp-F ${currentTemperatureUnit === "F" && "switch__active"}`}>F</p>
+      <p className={`switch__temp-C ${currentTemperatureUnit === "C" && "switch__active"}`}>C</p>
+    </label>
+  );
+}
+
+export default ToggleSwitch;
