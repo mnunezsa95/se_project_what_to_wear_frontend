62c9c9452dade823932be510b1c6037f8616883e
mnunezsa <mnunezsa95@gmail.com>
2023-07-28 12:35:53 -0400

    add handleDeleteButton prop to item modal component

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ItemModal.css";
 
-function ItemModal({ selectedCard, onClose }) {
+function ItemModal({ selectedCard, onClose, handleDeleteButton }) {
   return (
     <div className="modal">
       <div className="modal__content-card">
@@ -12,7 +12,7 @@ function ItemModal({ selectedCard, onClose }) {
             <p className="modal__info-name">{selectedCard.name}</p>
             <p className="modal__info-weather">Weather: {selectedCard.weather}</p>
           </div>
-          <button className="modal__delete-button" type="text">
+          <button className="modal__delete-button" type="button" onClick={handleDeleteButton}>
             Delete item
           </button>
         </div>
