49133695abd9b6b6082290af92fc3632ab3d20c2
mnunezsa <mnunezsa95@gmail.com>
2023-07-28 12:31:07 -0400

    add delete item card

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -7,9 +7,14 @@ function ItemModal({ selectedCard, onClose }) {
       <div className="modal__content-card">
         <img className="modal__image" src={selectedCard.link} alt={selectedCard.name} />
         <button className="modal__close-button-image" type="button" onClick={onClose} />
-        <div className="modal__info">
-          <p className="modal__info-name">{selectedCard.name}</p>
-          <p className="modal__info-weather">Weather: {selectedCard.weather}</p>
+        <div className="modal__info-section-container">
+          <div className="modal__info">
+            <p className="modal__info-name">{selectedCard.name}</p>
+            <p className="modal__info-weather">Weather: {selectedCard.weather}</p>
+          </div>
+          <button className="modal__delete-button" type="text">
+            Delete item
+          </button>
         </div>
       </div>
     </div>
