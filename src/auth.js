9c6a699f1a99b2b35df0939d990430d2fbfa6144
mnunezsa <mnunezsa95@gmail.com>
2023-09-20 18:09:58 -0400

    add auth.js

---
+++ src/auth.js
@@ -0,0 +1,21 @@
+const baseUrl = "http://localhost:3001";
+
+export const signUp = ({ email, password, name, avatar }) => {
+  fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, name, avatar }),
+  });
+};
+
+export const signIn = ({ email, password }) => {
+  fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+};
